ab641295872e182aeb732c8c383b992d8c1ecf31
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-23 21:57:39 +0300

    first html

--- src/App.js
+++ src/App.js
@@ -10,19 +10,17 @@ import Pagination from "./components/Pagination/Pagination";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              Card component will be placed here
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
