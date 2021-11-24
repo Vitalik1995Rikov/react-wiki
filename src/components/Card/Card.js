3925aaed66765543055bc8a3fc77eda5e65da90b
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-24 16:57:25 +0300

    add all cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,3 +1,5 @@
+import styles from './Card.module.scss';
+
 const Card = ({results}) => {
     let display;
 
@@ -5,10 +7,17 @@ const Card = ({results}) => {
         display = results.map((x) => {
             let {id, image, name, status, location} = x;
             return (
-                <div
-                key={id}
-                className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark"
-                >
+                <div key={id} className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark">
+                    <div className={`${styles.card} d-flex flex-column justify-content-center`}>
+                        <img className={`${styles.img} img-fluid`} src={image} alt="" />
+                        <div className={`${styles.content}`}>
+                            <div className="fs-5 fw-bold mb-4">{name}</div>
+                            <div className="">
+                                <div className="fs-6 fw-normal">Last Location</div>
+                                <div className="fs-5">{location.name}</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )
         })
@@ -18,3 +27,5 @@ const Card = ({results}) => {
     }
     return <>{display}</>
 }
+
+export default Card;
